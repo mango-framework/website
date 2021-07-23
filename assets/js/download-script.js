e4457fed33946ba57dc04aa835cdc1a7094fb8aa
mango-framework <87871740+mango-framework@users.noreply.github.com>
2021-07-23 22:51:01 +0400

    Add files via upload

--- assets/js/download-script.js
+++ assets/js/download-script.js
@@ -2,6 +2,18 @@ __download =`
     <div class="text">
         Download
     </div>
+
+    <div class="mango-cover">
+        <div class="form-text">
+            For <span class="mango" title="Mango's quick start pack">Mango Cutter</span> click to link and install :
+        </div>
+        <div class="mango-center">
+            <a class="mango-link-button" href="">
+                <i class="bx bx-download"></i> Download
+            </a>
+        </div>
+    </div>
+    
     <div class="mango-cover">
         <div class="form-text">
             For <span class="mango" title="Mango bundle">Mango Tree</span> Paste this code to among &lt;/body&gt; and &lt;/html&gt; :
@@ -13,10 +25,10 @@ __download =`
 
     <div class="mango-cover">
         <div class="form-text">
-            For <span class="mango" title="Mango's CSS infrastructure">Mango Juice</span> Paste this code to among &lt;/body&gt; and &lt;/html&gt; :
+            For <span class="mango" title="Mango's CSS infrastructure">Mango Juice</span> Paste this code to among &lt;head&gt; and &lt;/head&gt; :
         </div>
         <div class="code">
-            &lt;script href="Coming soon !" &gt;&lt;/script&gt;
+            &lt;link rel="stylesheet" href="https://mango-framework.github.io/mango/juice/mango-juice-pack.css" /&gt;
         </div>
     </div>
 
