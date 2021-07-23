20b51662443ca8badc99d5597f2064cfec581a81
mango-framework <87871740+mango-framework@users.noreply.github.com>
2021-07-23 20:41:14 +0400

    Add files via upload

---
+++ assets/js/download-script.js
@@ -0,0 +1,35 @@
+__download =`
+    <div class="text">
+        Download
+    </div>
+    <div class="mango-cover">
+        <div class="form-text">
+            For <span class="mango" title="Mango bundle">Mango Tree</span> Paste this code to among &lt;/body&gt; and &lt;/html&gt; :
+        </div>
+        <div class="code">
+            &lt;script href="Coming soon !" &gt;&lt;/script&gt;
+        </div>
+    </div>
+
+    <div class="mango-cover">
+        <div class="form-text">
+            For <span class="mango" title="Mango's CSS infrastructure">Mango Juice</span> Paste this code to among &lt;/body&gt; and &lt;/html&gt; :
+        </div>
+        <div class="code">
+            &lt;script href="Coming soon !" &gt;&lt;/script&gt;
+        </div>
+    </div>
+
+    <div class="mango-cover">
+        <div class="form-text">
+            For <span class="mango" title="Mango's Component infrastructure">Mango Seed</span> Paste this code to among &lt;/body&gt; and &lt;/html&gt; :
+        </div>
+        <div class="code">
+            &lt;script href="Coming soon !" &gt;&lt;/script&gt;
+        </div>
+    </div>
+`;
+
+function downloadScript() {
+    document.getElementById('downloadScript').innerHTML = __download;
+}
